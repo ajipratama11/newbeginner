2f73f549e6cee9e2f2adad01ddc9300ee0ae69fd
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2019-03-11 11:58:02 +0700

    selesai coba registration

--- wpu-login/application/controllers/Auth.php
+++ wpu-login/application/controllers/Auth.php
@@ -10,7 +10,8 @@ class Auth extends CI_Controller
     }
     public function index()
     {
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'Coba Login';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
@@ -18,7 +19,7 @@ class Auth extends CI_Controller
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [ 'is_unique' => 'This email used!' ] );
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
             'matches' => 'Password dont match!',
             'min_length' => 'Password too short!'
@@ -31,7 +32,21 @@ class Auth extends CI_Controller
         $this->load->view('auth/registration');
         $this->load->view('templates/auth_footer');
         } else {
-            echo 'data berhasil ditambahkan!';
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Registration Success, Please Login!
+          </div>');
+            redirect('auth');
         }
         
     }
