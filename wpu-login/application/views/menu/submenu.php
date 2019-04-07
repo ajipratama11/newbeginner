ae72bea33e7abf0c6d1498e5a1ae4bda11bcea2b
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2019-04-07 15:30:18 +0700

    access

---
+++ wpu-login/application/views/menu/submenu.php
@@ -0,0 +1,104 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg">
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newSubMenuModal">Add new Submenu</a>
+            <?php if (validation_errors()) : ?>
+                <div class="alert alert-danger" role="alert"><?= validation_errors(); ?></div>
+            <?php endif; ?>
+            <?= $this->session->flashdata('message'); ?>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Menu</th>
+                        <th scope="col">Url</th>
+                        <th scope="col">Icon</th>
+                        <th scope="col">Active</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($subMenu as $sm) : ?>
+                        <tr>
+                            <th scope="row"><?= $i; ?></th>
+                            <td><?= $sm['title']; ?></td>
+                            <td><?= $sm['menu']; ?></td>
+                            <td><?= $sm['url']; ?></td>
+                            <td><?= $sm['icon']; ?></td>
+                            <td><?= $sm['is_active']; ?></td>
+                            <td>
+                                <a href="#" class="badge badge-warning">edit</a>
+                                <a href="#" class="badge badge-danger">hapus</a>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+<!-- Modal -->
+<!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="newSubMenuModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add New Submenu</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= base_url('menu/submenu'); ?>" method="post">
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Menu name</label>
+                        <input type="text" class="form-control" id="title" name="title" placeholder="SubMenu name">
+                    </div>
+                    <div class="form-group">
+                        <select name="menu_id" id="menu_id" class="form-control">
+                            <option value="">Select Menu</option>
+                            <?php foreach ($menu as $m) : ?>
+                                <option value="<?= $m['id']; ?>"><?= $m['menu']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Url</label>
+                        <input type="text" class="form-control" id="url" name="url" placeholder="Url">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">icon</label>
+                        <input type="text" class="form-control" id="icon" name="icon" placeholder="icon">
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="1" id="is_active" name="is_active" checked>
+                            <label class="form-check-label" for="is_active">
+                                Active?
+                            </label>
+                        </div>
+                    </div>
+
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
