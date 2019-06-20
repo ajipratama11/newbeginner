ed789b4fef42be28a7ce5e9660a9fb79f10e6505
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2019-06-21 01:15:15 +0700

    x

--- TrackingCetol/app/src/main/java/com/example/trackingcetol/Rest/ApiClient.java
+++ TrackingCetol/app/src/main/java/com/example/trackingcetol/Rest/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class ApiClient {
-    public static final String BASE_URL = "http://192.168.43.234/cetol/index.php/ambilstatus/";
+    public static final String BASE_URL = "http://192.168.1.9/cetol/index.php/ambilstatus/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
